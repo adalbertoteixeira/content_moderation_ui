2f7b8ddd428abba543bc04727ecd134a8c5f6bdc
Adalberto Teixeira <adalberto.teixeira@cdigital.pt>
2025-03-06 19:17:50 +0000

    remove comments

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,14 +1,3 @@
-// import type { Handle } from '@sveltejs/kit';
-// import { sequence } from '@sveltejs/kit/hooks';
-// import { handleClerk } from 'clerk-sveltekit/server';
-// import { PRIVATE_CLERK_SECRET_KEY } from '$env/static/private';
 import { withClerkHandler } from 'svelte-clerk/server';
 
 export const handle = withClerkHandler();
-// export const handle: Handle = sequence(
-// 	handleClerk(PRIVATE_CLERK_SECRET_KEY, {
-// 		debug: true,
-// 		protectedPaths: ['/admin', '/dashboard'],
-// 		signInUrl: '/sign-in'
-// 	})
-// );
